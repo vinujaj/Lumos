a91e22f5fa3002610ce887da5f58b6a0730836e4
sulakshana bandara <sulakshanabandara2001@gmail.com>
2023-04-02 23:08:15 +0530

    added the signout button

--- Frontend_lumos/android/app/src/main/kotlin/com/example/frontend_lumos/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.frontend_lumos
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Frontend_lumos/android/app/src/main/kotlin/com/example/lumosappsdgp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lumosappsdgp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
