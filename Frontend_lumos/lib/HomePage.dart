17a5a9ab336870d05049535f1b6edfed9606adcf
sulakshana bandara <sulakshanabandara2001@gmail.com>
2023-04-02 21:59:23 +0530

    connected one(sula and thaalib)

---
+++ Frontend_lumos/lib/HomePage.dart
@@ -0,0 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/profile_page.dart';
+import 'pages/settings_page.dart';
+
+class InitialPage extends StatefulWidget {
+  const InitialPage({Key? key}) : super(key: key);
+
+  @override
+  State<InitialPage> createState() => _InitialPageState();
+}
+
+class _InitialPageState extends State<InitialPage> {
+  int currentIndex = 1;
+  final screens = <Widget>[
+    const ProfilePage(),
+    const HomePage(),
+    const SettingsPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue[500],
+        selectedItemColor: Colors.white,
+        showUnselectedLabels: false,
+        iconSize: 40,
+        currentIndex: currentIndex,
+        onTap: (index) => setState(() => currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+            backgroundColor: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+// Container(
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Text("sign in"),
+// MaterialButton(onPressed: (){
+// FirebaseAuth.instance.signOut();
+// })
+// ],
+// ),
+// ),
+// );
--- Frontend_lumos/lib/main.dart
+++ Frontend_lumos/lib/main.dart
@@ -1,79 +1,23 @@
 import 'package:flutter/material.dart';
-//import 'gesturebtnpage/main_gesture_page.dart';
-import 'home_page.dart';
-import 'profile_page.dart';
-import 'settings_page.dart';
+import 'main_page.dart';
+import 'pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Lumos App - Hand Gesture Recognition App',
       debugShowCheckedModeBanner: false,
-      
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: const MainPage(
-        title: 'Home Page',
-      ),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key, required String title});
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int currentIndex = 1;
-  final screens = <Widget>[
-    const ProfilePage(),
-    const HomePage(),
-    const SettingsPage()
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.blue[500],
-        selectedItemColor: Colors.white,
-        showUnselectedLabels: false,
-        iconSize: 40,
-        currentIndex: currentIndex,
-        onTap: (index) => setState(() => currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: "Profile",
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-            backgroundColor: Colors.red,
-          ),
-        ],
-      ),
+      home: MainPage(),
     );
   }
 }
