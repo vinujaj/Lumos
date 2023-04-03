634a0110c24c0a67dbd4febad713119fc8cff2d3
sulakshana bandara <sulakshanabandara2001@gmail.com>
2023-04-03 20:47:09 +0530

    updated the front end

--- Frontend_lumos/lib/pages/login_page.dart
+++ Frontend_lumos/lib/pages/login_page.dart
@@ -113,48 +113,48 @@ class _LoginPageState extends State<LoginPage> {
                   const SizedBox(height: 50),
 
                   // or continue with
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                          child: Text(
-                            'Or continue with',
-                            style: TextStyle(color: Colors.grey[700]),
-                          ),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.5,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  //   child: Row(
+                  //     children: [
+                  //       Expanded(
+                  //         child: Divider(
+                  //           thickness: 0.5,
+                  //           color: Colors.grey[400],
+                  //         ),
+                  //       ),
+                  //       Padding(
+                  //         padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  //         child: Text(
+                  //           'Or continue with',
+                  //           style: TextStyle(color: Colors.grey[700]),
+                  //         ),
+                  //       ),
+                  //       Expanded(
+                  //         child: Divider(
+                  //           thickness: 0.5,
+                  //           color: Colors.grey[400],
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
 
                   const SizedBox(height: 50),
 
                   // google + apple sign in buttons
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      // google button
-                      SquareTile(imagePath: 'lib/images/google.png'),
-
-                      SizedBox(width: 25),
-
-                      // apple button
-                      SquareTile(imagePath: 'lib/images/apple.png')
-                    ],
-                  ),
+                  // Row(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: const [
+                  //     // google button
+                  //     SquareTile(imagePath: 'lib/images/google.png'),
+                  //
+                  //     SizedBox(width: 25),
+                  //
+                  //     // apple button
+                  //     SquareTile(imagePath: 'lib/images/apple.png')
+                  //   ],
+                  // ),
 
                   const SizedBox(height: 20),
 
